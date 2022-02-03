bab1304508ece69ae9ad109cf460cc9be18fd0ad
Vishnudas P <vishnudaspunathil@gmail.com>
2022-02-04 00:08:15 +0530

    update

---
+++ SNFrshop/SNFrshop/src/test/java/com/example/SNFrshop/SnFrshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SNFrshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnFrshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
